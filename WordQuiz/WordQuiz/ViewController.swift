f2b84342bfc4bcb3b51c10462bfe64470f5c18c4
jonhpol <j.paulo_os@hotmail.com>
2019-12-13 21:37:53 -0300

    Setup project

---
+++ WordQuiz/WordQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordQuiz
+//
+//  Created by João Paulo de Oliveira Sabino on 13/12/19.
+//  Copyright © 2019 João Paulo de Oliveira Sabino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
