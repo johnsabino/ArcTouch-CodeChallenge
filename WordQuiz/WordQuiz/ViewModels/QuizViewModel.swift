c0325b5c0dcbcbe85f4f87a5b3b9c81930b9de05
jonhpol <j.paulo_os@hotmail.com>
2019-12-15 00:06:39 -0300

    Update MARK comment in QuizViewModel

--- WordQuiz/WordQuiz/ViewModels/QuizViewModel.swift
+++ WordQuiz/WordQuiz/ViewModels/QuizViewModel.swift
@@ -37,13 +37,14 @@ class QuizViewModel {
         }
     }
         
-    //MARK: - Quiz Logic
+    // MARK: - Quiz Logic
     
     /**
      Chech if the text is a valid answer.
      
      - parameter text: Input text
-     - parameter hittedAnswerCompletion: if is a valid text, call the completion with the answers count in correct format.
+     - parameter hittedAnswerCompletion: if is a valid text, call the completion with the     
+     answers count in correct format.
      - parameter countAnswers: The count of correct answers in format: NN/NN
     */
     func verify(text: String, hittedAnswerCompletion: (_ countAnswers: String) -> Void) {
